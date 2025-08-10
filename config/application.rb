5a8d1f40ed107d91bad33087b80189e2f20e535e
YurikOnRails <andrei.iurik@gmail.com>
2025-08-11 00:52:32 +0300

    feat: add Moscow time zone

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,9 @@ module DreamTeamSaas
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    # Time zone configuration
+    config.time_zone = 'Moscow'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
